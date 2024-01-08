ee6eac15e186b40ea8cb924ce5c952e89cb17fd9
mihaly-ale <105678319+mihaly-ale@users.github.com>
2024-01-08 09:50:07 +0100

    initilaize variables for app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+const calculator = document.querySelector('.calculator');
+const displayHistory = calculator.querySelector('.display-history');
+const displayCurrent = calculator.querySelector('.display-current');
+const keys = calculator.querySelector('.calculator__keys');
