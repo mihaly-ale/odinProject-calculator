8e077883fbacf41ccf71173aa14ab6f088551a55
mihaly-ale <105678319+mihaly-ale@users.github.com>
2024-01-08 13:04:27 +0100

    Add event listener for calculator keys
    Improved variable naming for better clarity
    Set logic for handling number and operator keys
    Added data attributes to index.html

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 const calculator = document.querySelector('.calculator');
-const displayHistory = calculator.querySelector('.display-history');
-const displayCurrent = calculator.querySelector('.display-current');
+const historyDisplay = calculator.querySelector('.display-history');
+const currentDisplay = calculator.querySelector('.display-current');
 const keys = calculator.querySelector('.calculator__keys');
+
+keys.addEventListener('click', (event) => {
+  const key = event.target;
+  const keyValue = key.textContent;
+  const currentDisplayValue = currentDisplay.textContent;
+  const historyDisplayValue = historyDisplay.textContent;
+  const { type } = key.dataset;
+
+  if (type === 'number') {
+    if (currentDisplayValue === '0') {
+      currentDisplay.textContent = keyValue;
+    } else if (calculator.dataset.previousKeyType === 'operator') {
+      currentDisplay.textContent = keyValue;
+    } else {
+      currentDisplay.textContent = currentDisplayValue + keyValue;
+    }
+  }
+
+  if (type === 'operator') {
+    console.log(`line 24: ${historyDisplayValue}, ${currentDisplayValue}`);
+
+    if (historyDisplayValue) {
+      historyDisplay.textContent =
+        historyDisplayValue + currentDisplayValue + keyValue;
+    } else {
+      historyDisplay.textContent = currentDisplayValue + keyValue;
+    }
+  }
+  calculator.dataset.previousKeyType = type;
+});
