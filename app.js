597195da41be2d7ab71ed35b86c69bfbfc555f03
mihaly-ale <105678319+mihaly-ale@users.github.com>
2024-01-08 18:05:40 +0100

    Add operator handling in click event
    Set calculator dataset operator property based on the last character of historyDisplayValue, e.g previous operation operator, calculator dataset operator takes current keyValue for the next operation.
    Initialized of variables(firstNumber, operator, secondNumber) for the calculation function
    Implemented basic calculation logic for multiplying and subtracting in claculate function

--- app.js
+++ app.js
@@ -21,14 +21,35 @@ keys.addEventListener('click', (event) => {
   }
 
   if (type === 'operator') {
-    console.log(`line 24: ${historyDisplayValue}, ${currentDisplayValue}`);
+    calculator.dataset.operator =
+      historyDisplayValue[historyDisplayValue.length - 1];
+    const operator = calculator.dataset.operator;
+    let secondNumber = currentDisplayValue;
 
     if (historyDisplayValue) {
+      calculator.dataset.firstNumber = historyDisplayValue;
+      const firstNumber = calculator.dataset.firstNumber.slice(0, -1);
       historyDisplay.textContent =
         historyDisplayValue + currentDisplayValue + keyValue;
+
+      currentDisplay.textContent = calculate(
+        firstNumber,
+        operator,
+        secondNumber
+      );
+      historyDisplay.textContent = currentDisplay.textContent + keyValue;
     } else {
       historyDisplay.textContent = currentDisplayValue + keyValue;
     }
+    calculator.dataset.operator = keyValue;
   }
   calculator.dataset.previousKeyType = type;
 });
+
+function calculate(firstNumber, operator, secondNumber) {
+  console.log(firstNumber, operator, secondNumber);
+  firstNumber = parseInt(firstNumber);
+  secondNumber = parseInt(secondNumber);
+  if (operator === '+') return firstNumber + secondNumber;
+  if (operator === '-') return firstNumber - secondNumber;
+}
